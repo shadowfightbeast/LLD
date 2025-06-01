539c79de215b9f7b30c121c1e7ab169852235879
Mithilesh kumar singh <mithileshkumarsingh39@gmail.com>
2025-06-01 12:24:58 +0530

    june code push 2

--- DESIGNS/BookMyShow/src/BookingStatus.java
+++
@@ -1,8 +0,0 @@
-package LLD_JAVA.BookMyShow.src;
-
-// Enum to represent the status of a booking
-enum BookingStatus {
-    CONFIRMED,
-    PENDING,
-    CANCELLED
-}
--- DESIGNS/BookMyShow/src/Customer.java
+++
@@ -1,4 +0,0 @@
-package LLD_JAVA.BookMyShow.src;
-public class Customer {
-
-}
